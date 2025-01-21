979b70a970ff120d60dd8a6188095327be2124d6
prostreet1212@gmail.com <ktndlf,tcfyu2020>
2025-01-21 23:01:15 +0300

    local html

--- lib/theme14/lesson119/main119.dart
+++ lib/theme14/lesson119/main119.dart
@@ -2,8 +2,12 @@
 import 'package:basics_2/theme14/lesson115/pages/webview_page.dart';
 import 'package:basics_2/theme14/lesson119/webview_local_page.dart';
 import 'package:flutter/material.dart';
-
-void main() => runApp(MyApp());
+import 'package:webview_flutter_plus/webview_flutter_plus.dart';
+LocalhostServer localhostServer = LocalhostServer();
+void main() async{
+  await localhostServer.start(port: 0);
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
--- lib/theme14/lesson119/webview_local_page.dart
+++ lib/theme14/lesson119/webview_local_page.dart
@@ -1,6 +1,10 @@
-import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:webview_flutter_plus/webview_flutter_plus.dart';
+
+import 'main119.dart';
 
 class WebViewLocalPage extends StatefulWidget {
   const WebViewLocalPage({super.key});
@@ -10,20 +14,57 @@ class WebViewLocalPage extends StatefulWidget {
 }
 
 class _WebViewLocalPageState extends State<WebViewLocalPage> {
-  late WebViewController _webController;
+  late WebViewControllerPlus _webController;
+
   @override
   void initState() {
     super.initState();
-    _webController=WebViewController()
-      ..setJavaScriptMode(JavaScriptMode.unrestricted);
+    _webController = WebViewControllerPlus(
+
+    )
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      //..loadRequest(Uri.parse('lib/theme14/lesson119/assets/index.html'))
+      //..loadFlutterAssetWithServer('lib/theme14/lesson119/assets/index.html', localhostServer.port!)
+    //..loadFile('lib/theme14/lesson119/assets/index.html')
+    //..loadFlutterAsset('lib/theme14/lesson119/assets/index.html')
+    ;
+    //loadLocalHTML();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('WebView Local'),
       ),
-      body: WebViewWidget(controller: _webController),
+      body: WebViewWidget(
+        controller: _webController,
+      ),
     );
+  }
+
+//   final html = '''<!DOCTYPE html>
+// <html>
+// <body>
+//
+// <h1>This is heading 1</h1>
+// <h2>This is heading 2</h2>
+// <h3>This is heading 3</h3>
+// <h4>This is heading 4</h4>
+// <h5>This is heading 5</h5>
+// <h6>This is heading 6</h6>
+//
+// </body>
+// </html>''';
+
+  Future<void> loadLocalHTML() async {
+    final html=await rootBundle.loadString('lib/theme14/lesson119/assets/index.html');
+    final url = Uri.dataFromString(
+      html,
+      mimeType: 'text/html',
+      encoding: Encoding.getByName('utf-8'),
+    ).toString();
+    _webController.loadRequest(Uri.parse(url));
+
   }
 }
